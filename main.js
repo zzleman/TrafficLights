c4fe835eac9497689c8f592cbbedaa5bf0831bb3
Leman <leman@Leman-MacBook-Air.local>
2023-05-12 08:41:08 +0400

    created

---
+++ main.js
@@ -0,0 +1,29 @@
+function startLights () {
+    let green=document.querySelector("#green");
+    let red=document.querySelector("#red");
+    let yellow=document.querySelector("#yellow");
+ 
+    green.style.opacity=1;
+    setTimeout(() => {
+        green.style.opacity=.2;
+        red.style.opacity=.2;
+        yellow.style.opacity=1;
+    },15000);
+ 
+    setTimeout(() => {
+        green.style.opacity=.2;
+        red.style.opacity=1;
+        yellow.style.opacity=.2;
+    },17000);
+ 
+    setTimeout(() =>  {
+        green.style.opacity=1;
+        red.style.opacity=.2;
+        yellow.style.opacity=.2;
+    },27000);
+}
+let countDown=setInterval( () => {
+    startLights();
+},27000);
+
+startLights();
